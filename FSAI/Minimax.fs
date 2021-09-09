f3bc9303a5abfd8e2aafdda9450d4ea168fd3916
ctaqvist <taca19sr@student.ju.se>
2021-09-09 09:54:17 +0200

    start
    created minimaxalphabeta function, not completed

--- FSAI/Minimax.fs
+++ FSAI/Minimax.fs
@@ -3,7 +3,35 @@
 module Minimax =
     let printHello message = 
         $"Hello, world {message}"
-
+            
     type Class1() = 
         member this.X = "F#"
+
+    let eval board = 
+        board
+
+    let Max x y = 
+        if x > y then
+            x
+        else
+            y
+
+    let Min x y = 
+        if y > x then
+            x
+        else
+            y
+
+    let miniMaxAlphaBeta board depth a b tile isMaxPlayer =
+        let bestScore = 0
+        //if depth = 0 then
+            //eval(board)
+        if isMaxPlayer then
+            bestScore = Max(bestScore, b)
+        else
+            bestScore = Min(bestScore, a)
+        if b <= a then
+            break
+            
+
     
